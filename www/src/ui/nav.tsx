4ed93a92f64b17a6888821d410afa73722887d79
Conor Davidson <conor@conordavidson.com>
2025-11-24 15:31:10 -0500

    Make dropdown nav

--- www/src/ui/nav.tsx
+++ www/src/ui/nav.tsx
@@ -14,6 +14,8 @@ import * as Types from '@/lib/types';
 import Icon from '@/ui/icon';
 import Link from 'next/link';
 
+import { NavigationMenu } from '@base-ui-components/react/navigation-menu';
+
 const Nav: React.FC = () => {
   const currentPath = Navigation.usePathname();
 
@@ -66,7 +68,14 @@ const Nav: React.FC = () => {
       className="sticky top-0 z-50 h-nav-height grid grid-cols-subgrid col-span-full bg-linear-to-b from-background from-30% to-transparent"
       onMouseLeave={() => setActiveMenuId(null)}
     >
-      <DesktopNav
+      {/* <DesktopNav
+        currentPath={currentPath}
+        visibleMenu={visibleMenu}
+        activeMenuId={activeMenuId}
+        onChangeActiveMenuId={setActiveMenuId}
+        onChangeLastActiveMenuId={setLastActiveMenuId}
+      /> */}
+      <DesktopNavMenu
         currentPath={currentPath}
         visibleMenu={visibleMenu}
         activeMenuId={activeMenuId}
@@ -85,6 +94,164 @@ const Nav: React.FC = () => {
     </nav>
   );
 };
+
+const DesktopNavMenu: React.FC<DesktopNavProps> = (props) => {
+  return (
+    <NavigationMenu.Root className="group/nav hidden lg:grid col-span-full grid-cols-subgrid">
+      <Page.Container className="h-nav-height flex relative z-50 items-center gap-x-4 justify-between">
+        <div className="flex items-center gap-2">
+          <Link href={Paths.HOME} className="flex items-center gap-2.5 hover:opacity-70">
+            <div className="h-[30px]">
+              <Graphics.Logomark />
+            </div>
+            <div
+              className={Utils.cx('h-[16px] transition-colors duration-50', {
+                // 'text-white': !!props.visibleMenu,
+              })}
+            >
+              <Graphics.Wordmark />
+            </div>
+          </Link>
+
+          <NavigationMenu.List className="pl-3 xl:pl-8 flex items-center gap-x-6 xl:gap-x-8">
+            {Content.NAVIGATION_MENU.map((item) => {
+              const isPathActive = props.currentPath === item.href;
+              const isMenuActive = props.visibleMenu?.label === item.label;
+
+              const content = () => {
+                return (
+                  <div className="relative">
+                    <div className="-z-10 absolute -top-1.5 left-0 right-0 flex justify-center">
+                      <div
+                        className={Utils.cx(
+                          'h-[4px] w-[4px] bg-brand opacity-0 group-data-popup-open:opacity-100 transition-all rounded-full'
+                        )}
+                      ></div>
+                    </div>
+                    <Text.Interface16
+                      className={Utils.cx(
+                        'cursor-pointer text-foreground group-hover:opacity-70 group-hover/nav-link:text-brand transition-all duration-50 group-data-popup-open:text-brand',
+                        {
+                          'text-white': !!props.visibleMenu,
+                          'text-brand': isPathActive || isMenuActive,
+                        }
+                      )}
+                    >
+                      {item.label}
+                    </Text.Interface16>
+                  </div>
+                );
+              };
+
+              if ('submenu' in item) {
+                return (
+                  <NavigationMenu.Item key={item.label} className="list-none">
+                    <NavigationMenu.Trigger className="group">{content()}</NavigationMenu.Trigger>
+                    <NavigationMenu.Content className={contentClassName}>
+                      <ul className="flex flex-col justify-center gap-y-4">
+                        {item.submenu.map((item) => (
+                          <li key={item.href} className="list-none">
+                            <Link
+                              key={item.label}
+                              href={item.href}
+                              className="w-fit relative group"
+                            >
+                              <div className="-z-10 absolute -top-1 -bottom-2 -left-4 -right-4 group-hover:bg-brand/15 blur-sm transition-colors rounded-xl"></div>
+                              <Text.Interface24
+                                className={Utils.cx('text-foreground transition-colors', {
+                                  'text-[#fa3d41]': props.currentPath === item.href,
+                                })}
+                              >
+                                {item.label}
+                              </Text.Interface24>
+                              {item.subheading && (
+                                <div className="mt-1">
+                                  <Text.Interface16 className={'text-muted'}>
+                                    {item.subheading}
+                                  </Text.Interface16>
+                                </div>
+                              )}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </NavigationMenu.Content>
+                  </NavigationMenu.Item>
+                );
+              }
+
+              return (
+                <Link key={item.label} href={item.href} className="group">
+                  <Text.Interface16
+                    className={Utils.cx(
+                      'cursor-pointer text-foreground group-hover:opacity-70 group-hover/nav-link:text-brand transition-all duration-50',
+                      {
+                        // 'text-white': !!props.visibleMenu,
+                        // 'text-brand': isPathActive || isMenuActive,
+                      }
+                    )}
+                  >
+                    {item.label}
+                  </Text.Interface16>
+                </Link>
+              );
+            })}
+          </NavigationMenu.List>
+          <NavigationMenu.Portal>
+            <NavigationMenu.Positioner
+              sideOffset={20}
+              collisionPadding={{ top: 5, bottom: 5, left: 20, right: 20 }}
+              collisionAvoidance={{ side: 'none' }}
+              className="box-border h-[var(--positioner-height)] w-[var(--positioner-width)] max-w-[var(--available-width)] transition-[top,left,right,bottom] duration-[var(--duration)] ease-[var(--easing)] before:absolute before:content-[''] data-[instant]:transition-none data-[side=bottom]:before:top-[-10px] data-[side=bottom]:before:right-0 data-[side=bottom]:before:left-0 data-[side=bottom]:before:h-2.5 data-[side=left]:before:top-0 data-[side=left]:before:right-[-10px] data-[side=left]:before:bottom-0 data-[side=left]:before:w-2.5 data-[side=right]:before:top-0 data-[side=right]:before:bottom-0 data-[side=right]:before:left-[-10px] data-[side=right]:before:w-2.5 data-[side=top]:before:right-0 data-[side=top]:before:bottom-[-10px] data-[side=top]:before:left-0 data-[side=top]:before:h-2.5 z-100"
+              style={{
+                ['--duration' as string]: '0.35s',
+                ['--easing' as string]: 'cubic-bezier(0.22, 1, 0.36, 1)',
+              }}
+            >
+              <NavigationMenu.Popup className="data-[ending-style]:easing-[ease] relative h-[var(--popup-height)] origin-[var(--transform-origin)] rounded-lg bg-emphasis dark-theme transition-[opacity,transform,width,height,scale,translate] duration-[var(--duration)] ease-[var(--easing)] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[ending-style]:duration-150 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 w-[var(--popup-width)] xs:w-[var(--popup-width)]">
+                <NavigationMenu.Arrow className="flex transition-[left] duration-[var(--duration)] ease-[var(--easing)] data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180">
+                  <ArrowSvg />
+                </NavigationMenu.Arrow>
+                <NavigationMenu.Viewport className="relative h-full w-full overflow-hidden" />
+              </NavigationMenu.Popup>
+            </NavigationMenu.Positioner>
+          </NavigationMenu.Portal>
+        </div>
+        <div>
+          <Button.Primary href={Paths.CONTACT}>Get in touch</Button.Primary>
+        </div>
+      </Page.Container>
+    </NavigationMenu.Root>
+  );
+};
+
+const ArrowSvg = (props: React.ComponentProps<'svg'>) => {
+  return (
+    <svg width="20" height="10" viewBox="0 0 20 10" fill="none" {...props}>
+      <path
+        d="M9.66437 2.60207L4.80758 6.97318C4.07308 7.63423 3.11989 8 2.13172 8H0V10H20V8H18.5349C17.5468 8 16.5936 7.63423 15.8591 6.97318L11.0023 2.60207C10.622 2.2598 10.0447 2.25979 9.66437 2.60207Z"
+        className="fill-emphasis"
+      />
+      <path
+        d="M8.99542 1.85876C9.75604 1.17425 10.9106 1.17422 11.6713 1.85878L16.5281 6.22989C17.0789 6.72568 17.7938 7.00001 18.5349 7.00001L15.89 7L11.0023 2.60207C10.622 2.2598 10.0447 2.2598 9.66436 2.60207L4.77734 7L2.13171 7.00001C2.87284 7.00001 3.58774 6.72568 4.13861 6.22989L8.99542 1.85876Z"
+        className="fill-gray-200 dark:fill-none"
+      />
+      <path
+        d="M10.3333 3.34539L5.47654 7.71648C4.55842 8.54279 3.36693 9 2.13172 9H0V8H2.13172C3.11989 8 4.07308 7.63423 4.80758 6.97318L9.66437 2.60207C10.0447 2.25979 10.622 2.2598 11.0023 2.60207L15.8591 6.97318C16.5936 7.63423 17.5468 8 18.5349 8H20V9H18.5349C17.2998 9 16.1083 8.54278 15.1901 7.71648L10.3333 3.34539Z"
+        className="dark:fill-gray-300"
+      />
+    </svg>
+  );
+};
+
+const contentClassName =
+  'h-full p-6 xs:w-max sm:min-w-[500px] xs:w-max ' +
+  'transition-[opacity,transform,translate] duration-[var(--duration)] ease-[var(--easing)] ' +
+  'data-[starting-style]:opacity-0 data-[ending-style]:opacity-0 ' +
+  'data-[starting-style]:data-[activation-direction=left]:translate-x-[-50%] ' +
+  'data-[starting-style]:data-[activation-direction=right]:translate-x-[50%] ' +
+  'data-[ending-style]:data-[activation-direction=left]:translate-x-[50%] ' +
+  'data-[ending-style]:data-[activation-direction=right]:translate-x-[-50%]';
 
 type MobileNavProps = {
   currentPath: string;
