460abeddeda364686d3f1b1117c9fd38994477f2
Conor Davidson <conor@conordavidson.com>
2025-11-25 15:37:48 -0500

    Fix recent insights

--- www/src/ui/sections/recentInsights.tsx
+++ www/src/ui/sections/recentInsights.tsx
@@ -28,64 +28,59 @@ const RecentInsights: React.FC<RecentInsightsProps> = async (props) => {
       <Page.Container>
         <Heading.Stack heading={props.section.heading} />
       </Page.Container>
-      <div className="mt-7 col-span-full">
-        <Marquee autoFill speed={20} pauseOnHover>
-          <div className="flex">
-            {recentPosts.map((post) => (
-              <Link
-                href={`/insights/${post.slug.current}`}
-                key={post._id}
-                className={Utils.cx('block w-[300px] cursor-pointer group mr-12')}
-              >
-                <div className="relative aspect-video rounded-xl overflow-hidden">
-                  <SanityImage
-                    image={post.coverImage.asset!}
-                    alt={post.coverImage.asset?.altText || ''}
-                    className="w-full h-full object-cover group-hover:scale-[1.01] transition-all duration-200 ease-in-out group-hover:brightness-110"
-                  />
-                  <div className="absolute inset-0 gradient-light z-10 opacity-100 group-hover:opacity-0 transition-opacity duration-200 ease-in-out"></div>
-
-                  <div className="absolute top-4 right-4 z-20 flex items-center gap-x-2">
-                    {post.tags?.map((tag) => (
+      <div className="mt-7 col-span-full flex overflow-x-auto pb-6 gap-x-5 snap-x snap-mandatory">
+        {recentPosts.map((post) => {
+          const firstTag = post.tags?.[0];
+          return (
+            <Link
+              href={`/insights/${post.slug.current}`}
+              key={post._id}
+              className={Utils.cx(
+                'block w-[300px] cursor-pointer group shrink-0 first:ml-gutter last:mr-gutter snap-start scroll-mx-gutter'
+              )}
+            >
+              <div className="relative aspect-video rounded-xl overflow-hidden">
+                <SanityImage
+                  image={post.coverImage.asset!}
+                  alt={post.coverImage.asset?.altText || ''}
+                  className="w-full h-full object-cover group-hover:scale-[1.01] transition-all duration-200 ease-in-out group-hover:brightness-110"
+                />
+                <div className="absolute inset-0 gradient-light z-10 opacity-100 group-hover:opacity-0 transition-opacity duration-200 ease-in-out"></div>
+              </div>
+              <div className="mt-4">
+                <div className="flex items-center justify-between gap-x-2">
+                  <Text.Interface14
+                    bold
+                    className="text-subtle group-hover:text-brand transition-colors"
+                  >
+                    {Utils.formatDate(post.publishedAt)}
+                  </Text.Interface14>
+                  <div></div>
+                  <div className="flex items-center gap-x-2">
+                    {firstTag && (
                       <div
-                        key={tag.title}
-                        className="bg-brand text-white rounded-md px-1 py-0.5 text-nowrap shadow-md"
+                        key={firstTag.slug.current}
+                        className="border border-brand rounded-md px-1 py-0.5"
                       >
-                        <Text.Interface12 key={tag.title} bold>
-                          {tag.title.toUpperCase()}
+                        <Text.Interface12 bold className="text-brand">
+                          {firstTag.title.toUpperCase()}
                         </Text.Interface12>
                       </div>
-                    ))}
+                    )}
                   </div>
                 </div>
-                <div className="mt-4">
-                  <div className="flex items-center justify-between gap-x-2">
-                    <Text.Interface14
-                      bold
-                      className="text-subtle group-hover:text-brand transition-colors"
-                    >
-                      {Utils.formatDate(post.publishedAt)}
-                    </Text.Interface14>
-                    <Text.Interface14
-                      bold
-                      className="text-subtle group-hover:text-brand transition-colors"
-                    >
-                      By {post.author.name}
-                    </Text.Interface14>
-                  </div>
-                  <div className="mt-3">
-                    <Text.Display24
-                      as="h3"
-                      className="text-muted italic text-pretty group-hover:text-brand transition-colors"
-                    >
-                      {post.title}
-                    </Text.Display24>
-                  </div>
+                <div className="mt-3">
+                  <Text.Display24
+                    as="h3"
+                    className="text-muted italic text-pretty group-hover:text-brand transition-colors"
+                  >
+                    {post.title}
+                  </Text.Display24>
                 </div>
-              </Link>
-            ))}
-          </div>
-        </Marquee>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
